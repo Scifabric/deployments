fa166305e97a79285d05f1356d769d9f401a4bcc
Daniel Lombraña González <teleyinex@gmail.com>
2015-02-10 17:01:20 +0100

    Raise errors based on failures in Ansible.

--- app.py
+++ app.py
@@ -92,6 +92,13 @@ def run_ansible_playbook(ansible_hosts, playbook):
                                    stats=stats, inventory=inventory)
     pb.run()
 
+
+    hosts = sorted(pb.stats.processed.keys())
+    for h in hosts:
+       t = pb.stats.summarize(h)
+       if t['failures'] > 0:
+           raise AnsibleError(t['failures'])
+
 def process_deployment(deployment):
     """Process deployment."""
     try:
